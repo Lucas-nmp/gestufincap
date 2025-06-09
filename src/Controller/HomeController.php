925996edfc52491194b0fe461a093cae2ce8e1fa
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-06-09 21:19:29 +0200

    Added news entity and part

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Repository\NewsRepository;
+
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(NewsRepository $newsRepository): Response
     {
+
+        $latestNews = $newsRepository->findBy([], ['id' => 'DESC'], 5);
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'latestNews' => $latestNews,
         ]);
     }
 }
---
+++ src/Entity/News.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsRepository::class)]
+class News
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $date_news = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $body = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $link = null;
+
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getDateNews(): ?\DateTime
+    {
+        return $this->date_news;
+    }
+
+    public function setDateNews(\DateTime $date_news): static
+    {
+        $this->date_news = $date_news;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): static
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
