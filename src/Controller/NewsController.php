c49aa5a1c47d7f5859f4455e97b8bb2c9379ff00
lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-06-16 20:42:41 +0200

    Route access control

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -11,8 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 #[Route('/news')]
 final class NewsController extends AbstractController
 {
